750be0efd033ded621bd12a2311b414b0ea4604c
kesiena <ohwotakesiena@gmail.com>
2022-06-22 07:09:31 -0700

    'hello-js'

---
+++ hello.js
@@ -0,0 +1,7 @@
+//Write a javascript code that prints out your name, height, and country on the screen
+
+const myName = 'Kesiena Cruz';
+const myHeight = '180m';
+const country = 'Nigeria';
+
+alert('My name is ' +myName+ ', am ' +myHeight+ ' tall and am from ' +country+ '.');
